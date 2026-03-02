de2e69672ab5c6e299a269ee99b962f5c7d7e76d
Christian F. Badillo H. <cris.badillo1408@gmail.com>
2026-03-01 20:48:02 -0600

    feat(utils/datasets): Add the Dataset Class HandwrittenData to assure Pytorch compatibility and apply transformations in batch

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,7 +1,11 @@
 import os
 import json
 from dataclasses import dataclass, field
-from typing import List, Dict
+from typing import Callable, List, Dict, Tuple, Optional
+import torch
+from torch.utils.data import Dataset
+from torchvision.transforms import ToTensor
+from PIL import Image
 
 
 @dataclass
@@ -25,3 +29,35 @@ def load_data(path: str) -> Datadict:
     temp_idx = list(temp_data.keys())
 
     return Datadict(data=temp_data, index=temp_idx)
+
+
+class HandwrittenData(Dataset):
+    def __init__(
+        self, datadict: Datadict, data_path: str, transform: Optional[Callable] = None
+    ) -> None:
+        if not os.path.exists(data_path):
+            raise FileNotFoundError(f"Not file o dir found: {data_path}")
+
+        super().__init__()
+        self.datadict = datadict
+        self.path = data_path
+        self.transform = transform
+        self.to_tensor = ToTensor()
+
+    def __len__(self) -> int:
+        return len(self.datadict.index)
+
+    def __getitem__(self, index: int) -> Tuple[torch.Tensor, str]:
+        file_name = self.datadict.index[index]
+        label = self.datadict.data.get(file_name, "")
+
+        full_path = os.path.join(self.path, file_name)
+        img = Image.open(full_path)
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if not isinstance(img, torch.Tensor):
+            img = self.to_tensor(img)
+
+        return img, label
